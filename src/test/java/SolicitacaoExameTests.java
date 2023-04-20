eb9445fbf7fff6bb4835b5f569b97a41a5275e70
Laura C <laurafariacastro@outlook.com>
2023-04-20 00:03:06 -0300

    unit tests

---
+++ src/test/java/SolicitacaoExameTests.java
@@ -0,0 +1,52 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class SolicitacaoExameTests {
+
+    @Test
+    void deveRetornarExcecaoCorreta(){
+        try {
+            SolicitacaoExame corpoHumano = new SolicitacaoExame();
+            corpoHumano.getPartes();
+            fail();
+        } catch (NullPointerException ex){
+            assertEquals("Exame sem partes internas para analisar!", ex.getMessage());
+        }
+    }
+
+    @Test
+    void deveRetornarSolicitacaoDeExameCorretas(){
+        SistemaHumano respiratorio = new SistemaHumano("Sistema Respiratorio");
+        Orgao pulmao = new Orgao("Pulmao");
+        respiratorio.addParteInterna(pulmao);
+
+        SistemaHumano digestorio = new SistemaHumano("Sistema Digestorio");
+        Orgao estomago = new Orgao("Estomago");
+        Orgao intestinoDelgado = new Orgao("Intestino delgado");
+        digestorio.addParteInterna(estomago);
+        digestorio.addParteInterna(intestinoDelgado);
+
+        SistemaHumano sistemaNervoso = new SistemaHumano("Sistema Nervoso");
+        Orgao cerebro = new Orgao("Cerebro");
+        sistemaNervoso.addParteInterna(cerebro);
+
+        SistemaHumano geral = new SistemaHumano("Corpo Humano");
+        geral.addParteInterna(respiratorio);
+        geral.addParteInterna(digestorio);
+        geral.addParteInterna(sistemaNervoso);
+
+        SolicitacaoExame corpoHumano = new SolicitacaoExame();
+        corpoHumano.setPartes(geral);
+
+        assertEquals("Parte: Corpo Humano\n" +
+                "Parte: Sistema Respiratorio\n" +
+                "Orgao: Pulmao\n" +
+                "Parte: Sistema Digestorio\n" +
+                "Orgao: Estomago\n" +
+                "Orgao: Intestino delgado\n" +
+                "Parte: Sistema Nervoso\n" +
+                "Orgao: Cerebro\n", corpoHumano.getPartes().getParteInterna());
+
+    }
+}
